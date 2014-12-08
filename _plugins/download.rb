020e5e5ce93365e093f5f2a9bfaefb61f3923365
Alistair Lynn <arplynn@gmail.com>
2014-12-08 14:12:26 +0000

    Use local copies of jQuery and jQuery UI

---
+++ _plugins/download.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+
+module DownloadPlugin
+    class DownloadConverter < Jekyll::Converter
+        def matches(ext)
+            ext =~ /^\.download$/i
+        end
+
+        def output_ext(ext)
+            ''
+        end
+
+        def convert(content)
+            uri = URI(content.chomp)
+            open(uri).read
+        end
+    end
+end
+
