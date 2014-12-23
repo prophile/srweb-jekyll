4931a5b80dca833aa6662ee7fff63cc8b66286e2
Alistair Lynn <arplynn@gmail.com>
2014-12-23 03:37:36 +0000

    Use bundle exec

--- utils/create_rpm.sh
+++ utils/create_rpm.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -e
+FPM="bundle exec fpm"
 mkdir -p _dist
 MAINTAINER=$(git show HEAD --pretty='%aN <%aE>' -s)
 VERSION=$(git describe --always)
-fpm -s dir -t rpm \
-    -n srweb \
-    -C _site \
-    --force \
-    --license MIT \
-    --version "$VERSION" \
-    --vendor "Student Robotics" \
-    --maintainer "$MAINTAINER" \
-    --description "Student Robotics website content" \
-    --architecture all \
-    --url 'https://www.studentrobotics.org/cgit/srweb.git/' \
-    --rpm-os linux \
-    --rpm-use-file-permissions \
-    --rpm-user wwwcontent \
-    --rpm-group apache \
-    --package "_dist/srweb-$VERSION.rpm" \
-    .=/var/www/html
+$FPM -s dir -t rpm \
+     -n srweb \
+     -C _site \
+     --force \
+     --license MIT \
+     --version "$VERSION" \
+     --vendor "Student Robotics" \
+     --maintainer "$MAINTAINER" \
+     --description "Student Robotics website content" \
+     --architecture all \
+     --url 'https://www.studentrobotics.org/cgit/srweb.git/' \
+     --rpm-os linux \
+     --rpm-use-file-permissions \
+     --rpm-user wwwcontent \
+     --rpm-group apache \
+     --package "_dist/srweb-$VERSION.rpm" \
+     .=/var/www/html
 
