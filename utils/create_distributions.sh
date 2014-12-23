5d47af648d20abb5cc677b3408c9c0e9b461ca2d
Alistair Lynn <arplynn@gmail.com>
2014-12-23 04:12:12 +0000

    Generate a tar.gz distribution as well as an RPM

---
+++ utils/create_distributions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+FPM="bundle exec fpm"
+mkdir -p _dist
+MAINTAINER=$(git show HEAD --pretty='%aN <%aE>' -s)
+VERSION=$(git describe --always)
+function package {
+    $FPM -s dir \
+         -n srweb \
+         -C _site \
+         --force \
+         --license MIT \
+         --version "$VERSION" \
+         --vendor "Student Robotics" \
+         --maintainer "$MAINTAINER" \
+         --description "Student Robotics website content" \
+         --architecture all \
+         --url 'https://www.studentrobotics.org/cgit/srweb.git/' \
+         $*
+}
+package -t rpm \
+        --rpm-os linux \
+        --rpm-use-file-permissions \
+        --rpm-user wwwcontent \
+        --rpm-group apache \
+        --package "_dist/srweb-$VERSION.rpm" \
+        .=/var/www/html
+package -t tar \
+        --package "_dist/srweb-$VERSION.tar.gz" \
+        .="/srweb-$VERSION"
+
