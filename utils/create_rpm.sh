2b3718b79a66f8e6f9bfcfb9f970ddc16ba2ab7e
Alistair Lynn <arplynn@gmail.com>
2014-12-23 03:30:59 +0000

    Add RPM generation script

---
+++ utils/create_rpm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+mkdir -p _dist
+MAINTAINER=$(git show HEAD --pretty='%aN <%aE>' -s)
+VERSION=$(git describe --always)
+fpm -s dir -t rpm \
+    -n srweb \
+    -C _site \
+    --force \
+    --license MIT \
+    --version "$VERSION" \
+    --vendor "Student Robotics" \
+    --maintainer "$MAINTAINER" \
+    --description "Student Robotics website content" \
+    --architecture all \
+    --url 'https://www.studentrobotics.org/cgit/srweb.git/' \
+    --rpm-os linux \
+    --rpm-use-file-permissions \
+    --rpm-user wwwcontent \
+    --rpm-group apache \
+    --package "_dist/srweb-$VERSION.rpm" \
+    .=/var/www/html
+
