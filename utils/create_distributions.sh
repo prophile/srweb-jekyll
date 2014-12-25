96e0baa0940d9ff5c19ee8eb03095faf2c95569d
Alistair Lynn <arplynn@gmail.com>
2014-12-25 03:00:36 +0000

    Use numerically increasing package numbers

--- utils/create_distributions.sh
+++ utils/create_distributions.sh
@@ -3,7 +3,16 @@ set -e
 FPM="bundle exec fpm"
 mkdir -p _dist
 MAINTAINER=$(git show HEAD --pretty='%aN <%aE>' -s)
-VERSION=$(git describe)
+if [ -n "$CI_PULL_REQUESTS" ]; then
+    VERSION="pr"
+    RELEASE="$CIRCLE_BUILD_NUM"
+elif [ -n "$CIRCLE_BRANCH" ]; then
+    VERSION=$(echo "$CIRCLE_BRANCH" | tr '[:lower:]' '[:upper:]')
+    RELEASE="$CIRCLE_BUILD_NUM"
+else
+    VERSION="dev"
+    RELEASE=$(git describe 2>/dev/null || (git rev-list HEAD | wc -l | tr -d ' '))
+fi
 function package {
     $FPM -s dir \
          -n srweb \
@@ -11,6 +20,7 @@ function package {
          --force \
          --license MIT \
          --version "$VERSION" \
+         --iteration "$RELEASE" \
          --vendor "Student Robotics" \
          --maintainer "$MAINTAINER" \
          --description "Student Robotics website content" \
@@ -23,12 +33,12 @@ package -t rpm \
         --rpm-use-file-permissions \
         --rpm-user wwwcontent \
         --rpm-group apache \
-        --package "_dist/srweb-$VERSION.rpm" \
+        --package "_dist/srweb-$VERSION-$RELEASE.rpm" \
         -d shadow-utils \
         --before-install utils/create_user_fedora.sh \
         --directories /var/www/html \
         .=/var/www/html
 package -t tar \
-        --package "_dist/srweb-$VERSION.tar.gz" \
-        .="/srweb-$VERSION"
+        --package "_dist/srweb-$VERSION-$RELEASE.tar.gz" \
+        .="/srweb-$VERSION-$RELEASE"
 
