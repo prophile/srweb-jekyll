0a5ed85f2b869c139e657748356449896d148417
Alistair Lynn <arplynn@gmail.com>
2014-12-27 01:28:52 +0000

    Add docs import script

---
+++ utils/import_all_docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+OUTPUT_DIR=`pwd`/docs
+PYTHON_UTIL=`pwd`/utils/import_srweb.py
+DOCS_DIR=$1
+cd $DOCS_DIR
+find . -not -name '.*' -type d | cut -c3- | xargs -I @ mkdir -p $OUTPUT_DIR/@
+find . -not -name '.*' -type f | cut -c3- | xargs -I @ python3 $PYTHON_UTIL -s -l docs @ -o $OUTPUT_DIR/@.md
+
